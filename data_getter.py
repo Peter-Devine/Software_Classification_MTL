a056ddc724a42f72b19a1e2322cdc7cf10d3ca95
Peter Devine <pdev438@UoA.auckland.ac.nz>
2020-02-20 17:38:42 +1300

    refactor data_getter into class

--- data_getter.py
+++ data_getter.py
@@ -1,31 +1,37 @@
-DATASET_TO_DOWNLOADER = {
-    "maalej_2015": get_maalej_2015
-}
+import requests, zipfile, io, os, json
+import pandas as pd
 
-def get_selected_datasets(names_of_datasets):
-    retreived_datasets = {}
-    for dataset_name in names_of_datasets:
-        retreived_dataset = DATASET_TO_DOWNLOADER[dataset_name]()
-        retreived_datasets[dataset_name] = retreived_dataset
+class DataGetter:
+    def __init__(self):
+        self.dataset_downloader_dict = {
+            "maalej_2015": self.get_maalej_2015
+        }
 
-    return retreived_datasets
+    def get_selected_datasets(self, names_of_datasets):
+        retreived_datasets = {}
+        for dataset_name in names_of_datasets:
+            retreived_dataset = self.dataset_downloader_dict[dataset_name]()
+            retreived_datasets[dataset_name] = retreived_dataset
 
-def get_maalej_2015():
+        return retreived_datasets
 
-    import requests, zipfile, io
 
-    if not os.path.exists("REJ_data.zip"):
-        r = requests.get("https://mast.informatik.uni-hamburg.de/wp-content/uploads/2014/03/REJ_data.zip")
-        z = zipfile.ZipFile(io.BytesIO(r.content))
-        z.extractall()
+    ######### INDIVIDUAL DATA GETTERS ############
 
-    # from https://mast.informatik.uni-hamburg.de/wp-content/uploads/2015/06/review_classification_preprint.pdf
-    # Bug Report, Feature Request, or Simply Praise? On Automatically Classifying App Reviews
-    json_path = "./REJ_data/all.json"
+    def get_maalej_2015(self):
 
-    with open(json_path) as json_file:
-        data = json.load(json_file)
+        if not os.path.exists("REJ_data.zip"):
+            r = requests.get("https://mast.informatik.uni-hamburg.de/wp-content/uploads/2014/03/REJ_data.zip")
+            z = zipfile.ZipFile(io.BytesIO(r.content))
+            z.extractall()
 
-    df = pd.DataFrame({"text": [x["title"] + " [SEP] " + x["comment"] if x["title"] is not None else " [SEP] " + x["comment"] for x in data], "label":[x["label"] for x in data]})
+        # from https://mast.informatik.uni-hamburg.de/wp-content/uploads/2015/06/review_classification_preprint.pdf
+        # Bug Report, Feature Request, or Simply Praise? On Automatically Classifying App Reviews
+        json_path = "./REJ_data/all.json"
 
-    return df
+        with open(json_path) as json_file:
+            data = json.load(json_file)
+
+        df = pd.DataFrame({"text": [x["title"] + " [SEP] " + x["comment"] if x["title"] is not None else " [SEP] " + x["comment"] for x in data], "label":[x["label"] for x in data]})
+
+        return df
