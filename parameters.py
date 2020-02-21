9181e4eb903ebfad0c4ab95009640d1bdd12b3f4
Peter Devine <pdev438@UoA.auckland.ac.nz>
2020-02-21 17:02:42 +1300

    Add more parameters to parameters class

--- parameters.py
+++ parameters.py
@@ -1,7 +1,24 @@
-class Hyperparameters:
-    def __init__(self, lm_model_name, max_length, batch_size_train, batch_size_eval, cv_folds):
+class Parameters:
+    def __init__(self,
+                lm_model_name,
+                max_length,
+                batch_size_train,
+                batch_size_eval,
+                learning_rate,
+                epsilon,
+                weight_decay,
+                early_stopping_patience,
+                num_epochs,
+                num_fine_tuning_epochs,
+                random_state):
         self.lm_model_name = lm_model_name
         self.max_length = max_length
         self.batch_size_train = batch_size_train
         self.batch_size_eval = batch_size_eval
-        self.cv_folds = cv_folds
+        self.learning_rate = learning_rate
+        self.epsilon = epsilon
+        self.weight_decay = weight_decay
+        self.early_stopping_patience = early_stopping_patience
+        self.num_epochs = num_epochs
+        self.num_fine_tuning_epochs = num_fine_tuning_epochs
+        self.random_state = random_state
