ee60d0474a37a7fb01598a55187922cf571c5c7b
Peter Devine <pdev438@UoA.auckland.ac.nz>
2020-02-24 17:52:49 +1300

    Add training code to train and log in multi-task and fine-tuning settings

--- ml_training.py
+++ ml_training.py
@@ -1,36 +1,78 @@
-metrics = []
+import random
+from evaluation_engines import create_eval_engine
+from model_saver import ModelSaver
 
-best_model = None
+def train_on_tasks(task_dict, PARAMS, logger, is_fine_tuning):
+    model_saver = ModelSaver(".")
 
-eval_engine = get_eval_engine_for_model(cls_lm)
+    metrics = []
 
-step_num = 0
+    task_eval_metrics = {task_name: [0] for task_name, task in task_dict.items()}
+    task_steps = {task_name: 0 for task_name, task in task_dict.items()}
+    task_eval_engines = {task_name: create_eval_engine(model=task.model, is_multilabel=task.is_multilabel) for task_name, task in task_dict.items()}
 
-for epoch in range(epochs):
-    for X, y in train_data:
-        logits = cls_lm(X.cuda())
-        loss_fct = torch.nn.CrossEntropyLoss()
-        loss = loss_fct(logits.view(-1, cls_lm.n_classes), y.cuda().view(-1))
-        loss.backward()
-        neptune.log_metric(log_name="split_num " + str(split_num) + ' - loss', x=step_num, y=loss.item())
-        optimizer.step()
-        cls_lm.zero_grad()
-        step_num += 1
+    task_training_list = []
+    for task_name, task in task_dict.items():
+        task_training_list.extend([task_name]*task.train_length)
 
-    eval_results = eval_engine.run(valid_data)
+    random.seed(PARAMS.random_state)
 
-    write_results(eval_results, split_num, epoch=epoch)
+    if not is_fine_tuning:
+        # Shuffle task list during multi-task training so that tasks are trained roughly evenly throughout
+        random.shuffle(task_training_list)
 
-    print(eval_results.metrics["confusion_matrix"].numpy())
+    step_num = 0
+    run_type_log_prefix = "Fine-tuning " if is_fine_tuning else "Multi-task training "
 
-    if eval_results.metrics["accuracy"] > max([0] + [x["accuracy"] for x in metrics]):
-        best_model = copy.deepcopy(cls_lm)
+    for epoch in range(epochs):
+        # TRAIN
+        for task_name in task_training_list:
+            task = task_dict[task_name]
 
-    metrics.append(eval_results.metrics)
+            loss_fn = task.loss_fn
 
-test_engine = get_eval_engine_for_model(best_model)
-test_results = test_engine.run(test_data)
+            X, y  = next(task.train_data)
 
-write_results(test_results, split_num, epoch=None)
+            logits = task.model(X.cuda())
+            loss_fn = task.loss_fn()
+            loss = loss_fn(logits.view(-1, task.n_classes), y.cuda().view(-1))
+            loss.backward()
+            task.optimizer.step()
+            task.model.zero_grad()
 
-split_results.append(test_results.metrics)
+            logger.log_metric(f'{run_type_log_prefix} {task_name} - loss', x=task_steps[task_name], y=loss.item())
+
+            # Only log overall loss when the tasks have a shared language model layer. During fine tuning, their models are no longer shared, making this metric useless.
+            if not is_fine_tuning:
+                logger.log_metric(f'{run_type_log_prefix} overall loss', x=step_num, y=loss.item())
+
+            step_num += 1
+            task_steps[task_name] += 1
+
+        # VALIDATE
+        for task_name, task in task_dict.items():
+            validation_results = task_eval_engines[task_name].run(task.valid_data).metrics
+            logger.log_results(run_type_log_prefix + task_name, "valid", epoch, validation_results)
+
+            # What metric will we compare all previous performance against
+            comparison_metric = validation_results["accuracy"]
+
+            if comparison_metric > max(task_eval_metrics[task_name]):
+                model_saver.save_model(file_name=task_name, task.model)
+
+            task_eval_metrics[task_name].append(comparison_metric)
+
+    # TEST
+    task_test_metrics = {task_name: None for task_name, task in task_dict.items()}
+    for task_name, task in task_dict.items():
+        task.model = model_saver.load_model(file_name=task_name)
+
+        test_engine = create_eval_engine(model=best_model, is_multilabel=task.is_multilabel)
+        test_results = test_engine.run(task.test_data).metrics
+
+        task_test_metrics[task_name] = test_results
+
+        epoch = 1 if is_fine_tuning else 0
+        logger.log_results(run_type_log_prefix + task_name, "test", epoch, test_results)
+
+    return task_eval_metrics, task_test_metrics
--- software_feedback_cls.py
+++ software_feedback_cls.py
@@ -3,6 +3,8 @@ from task_builder import TaskBuilder
 from data_processor_cv import get_cross_validated_df
 #from baselines import get_NB_baseline_eval_metrics
 from parameters import Parameters
+from ml_training import train_on_tasks
+from logger import NeptuneLogger
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--dataset_list', required=True, type=str, help='Comma separated list of datasets (E.g. "maalej_2015,chen_2014"). No spaces between datasets.')
@@ -17,11 +19,14 @@ parser.add_argument('--LR', default=5e-5, type=int, help='Learning rate for the 
 parser.add_argument('--EPS', default=1e-6, type=int, help='Epsilon of the model')
 parser.add_argument('--WD', default=0.01, type=int, help='Weight decay of the model')
 parser.add_argument('--random_state', default=42, type=int, help='Random state of the experiment (default 42)')
+parser.add_argument('--neptune_username', default="", type=str, help=' (Optional) For outputting training/eval metrics to neptune.ai. Valid neptune username. Not your neptune.ai API key must also be stored as $NEPTUNE_API_TOKEN environment variable.')
+
 args = parser.parse_args()
 print(args)
 
 dataset_list = args.dataset_list.split(",")
-PARAMS = Parameters(lm_model_name = args.model_name,
+PARAMS = Parameters(dataset_name_list = dataset_list,
+                    lm_model_name = args.model_name,
                     max_length = args.max_length,
                     batch_size_train = args.batch_size_train,
                     batch_size_eval = args.batch_size_eval,
@@ -33,32 +38,26 @@ PARAMS = Parameters(lm_model_name = args.model_name,
                     num_fine_tuning_epochs = args.num_fine_tuning_epochs,
                     random_state = args.random_state)
 
-if args.do_cv and len(dataset_list) > 1:
-    raise Exception("Cannot do literature comparison in multi-task setting")
+logger = NeptuneLogger(args.neptune_username)
+logger.create_experiment(PARAMS)
 
 task_builder = TaskBuilder(random_state=PARAMS.random_state)
 
 task_dict = task_builder.build_tasks(dataset_list, PARAMS)
 
-for task_name, task in task_dict:
-    train_df, valid_df, test_df = dataset.dataset
+#Do multi-task learning if more than one task is supplied
+if len(dataset_list) > 1:
+    task_eval_metrics, task_test_metrics = train_on_tasks(task_dict, PARAMS, logger, is_fine_tuning=False)
 
+# Fine tune on each task individually
+ft_task_eval_metrics, ft_task_test_metrics = train_on_tasks(task_dict, PARAMS, logger, is_fine_tuning=True)
 
-    dataset_name = selected_datasets.keys()[0]
-
-    dataset = selected_datasets[dataset_name]
-
-    cv_datasets = get_cross_validated_df(dataset, PARAMS.cv_folds, PARAMS.random_state)
-
-    model = get_model[dataset_name] ###TODO
-
-    cv_results = []
-
-    for cv_dataset in cv_datasets:
-        create_task(dataset_name, dataset_name)
-        pass
-else:
-
-### THEN ITERATE OVER CVs if applicable
-
-### ELSE JUST DO TRAINING ON GIVEN TVT SPLIT
+# Output final results to disk
+with open("./task_eval_metrics.txt","w") as f
+    f.write( str(task_eval_metrics) )
+with open("./task_test_metrics.txt","w") as f
+    f.write( str(task_test_metrics) )
+with open("./ft_task_eval_metrics.txt","w") as f
+    f.write( str(ft_task_eval_metrics) )
+with open("./ft_task_test_metrics.txt","w") as f
+    f.write( str(ft_task_test_metrics) )
