e40c76e514e4e0272a11336f345665d94919bece
Peter Devine <pdev438@UoA.auckland.ac.nz>
2020-03-04 16:16:52 +1300

    Add metrics that output arrays neatly for multilabel

--- evaluation_engines.py
+++ evaluation_engines.py
@@ -21,14 +21,14 @@ class TopK(Metric):
 
         if self.top_k_values is None:
             top_k = torch.topk(torch.softmax(y_pred, dim=1)[:,self.label_idx_of_interest], self.k, dim=0)
-            self.top_k_values = top_k.values
-            self.top_k_labels = y[top_k.indices]
+            self.top_k_values = top_k.values.cpu().numpy()
+            self.top_k_labels = y[top_k.indices].cpu().numpy()
         else:
             concatenated_values = torch.cat((torch.softmax(y_pred, dim=1)[:,self.label_idx_of_interest], self.top_k_values), dim=0)
             concatenated_labels = torch.cat((y, self.top_k_labels), dim=0)
             top_k = torch.topk(concatenated_values, self.k, dim=0)
-            self.top_k_values = top_k.values
-            self.top_k_labels = concatenated_labels[top_k.indices]
+            self.top_k_values = top_k.values.cpu().numpy()
+            self.top_k_labels = concatenated_labels[top_k.indices].cpu().numpy()
 
     def compute(self):
         assert len(self.top_k_labels) == self.k, f"There are {self.top_k_labels} labels when there should be {self.k} labels when calculating top k labels"
@@ -36,99 +36,139 @@ class TopK(Metric):
         percentage_in_top_k = float(number_correct) / float(self.k)
         return percentage_in_top_k
 
-class MulticlassPrecisionRecall(Metric):
-    def __init__(self, output_transform=lambda x: x, label_idx_of_interest=0):
+class CountHolder():
+    def __init__(self):
         self.fp = 0
         self.fn = 0
         self.tp = 0
         self.tn = 0
-        self.label_idx_of_interest = label_idx_of_interest
+
+    def update(self, fp, fn, tp, tn):
+        self.fp += fp
+        self.fn += fn
+        self.tp += tp
+        self.tn += tn
+
+class MulticlassPrecisionRecall(Metric):
+    def __init__(self, output_transform=lambda x: x, n_classes=2):
+        self.class_count = []
+        for i in range(n_classes):
+            self.class_count[i] = CountHolder()
+
+        self.n_classes = n_classes
         self.sigmoid_fn = torch.nn.Sigmoid()
         super(MulticlassPrecisionRecall, self).__init__(output_transform=output_transform)
 
     def reset(self):
-        self.fp = 0
-        self.fn = 0
-        self.tp = 0
-        self.tn = 0
+        for i in range(self.n_classes):
+            self.class_count[i] = CountHolder()
 
     def update(self, output):
-        y_pred, y = output
-        y_pred = y_pred[:, self.label_idx_of_interest]
-        y_pred = self.sigmoid_fn(y_pred)
-        y_pred = y_pred.round()
-        y = y[:, self.label_idx_of_interest]
-        self.fp += int(((y_pred == 1) & (y == 0)).sum())
-        self.fn += int(((y_pred == 0) & (y == 1)).sum())
-        self.tp += int(((y_pred == 1) & (y == 1)).sum())
-        self.tn += int(((y_pred == 0) & (y == 0)).sum())
+        y_pred_all, y_all = output
+
+        for i in range(self.n_classes):
+            y_pred = y_pred_all[:, i]
+            y_pred = self.sigmoid_fn(y_pred)
+            y_pred = y_pred.round()
+            y = y_all[:, i]
+            fp = int(((y_pred == 1) & (y == 0)).sum())
+            fn = int(((y_pred == 0) & (y == 1)).sum())
+            tp = int(((y_pred == 1) & (y == 1)).sum())
+            tn = int(((y_pred == 0) & (y == 0)).sum())
+
+            for i in range(self.n_classes):
+                self.class_count[i].update(fp=fp, fn=fn, tp=tp, tn=tn)
 
 class MulticlassPrecision(MulticlassPrecisionRecall):
-    def __init__(self, output_transform=lambda x: x, label_idx_of_interest=0):
-        super(MulticlassPrecision, self).__init__(output_transform=output_transform, label_idx_of_interest=label_idx_of_interest)
+    def __init__(self, output_transform=lambda x: x, n_classes=2):
+        super(MulticlassPrecision, self).__init__(output_transform=output_transform, n_classes=n_classes)
 
     def compute(self):
-        if (self.tp + self.fp) == 0:
-            return np.nan
-        else:
-            return self.tp / (self.tp + self.fp)
+        results = []
+        for i in range(self.n_classes):
+            tp = self.class_count[i].tp
+            fp = self.class_count[i].fp
+            if (tp + fp) == 0:
+                results.append(np.nan)
+            else:
+                results.append(tp / (tp + fp))
+        return results
 
 class MulticlassRecall(MulticlassPrecisionRecall):
-    def __init__(self, output_transform=lambda x: x, label_idx_of_interest=0):
-        super(MulticlassRecall, self).__init__(output_transform=output_transform, label_idx_of_interest=label_idx_of_interest)
+    def __init__(self, output_transform=lambda x: x, n_classes=2):
+        super(MulticlassRecall, self).__init__(output_transform=output_transform, n_classes=n_classes)
 
     def compute(self):
-        if (self.tp + self.fn) == 0:
-            return np.nan
-        else:
-            return self.tp / (self.tp + self.fn)
+        results = []
+        for i in range(self.n_classes):
+            tp = self.class_count[i].tp
+            fn = self.class_count[i].fn
+            if (tp + fn) == 0:
+                results.append(np.nan)
+            else:
+                results.append(tp / (tp + fn))
+        return results
 
 class MulticlassF1(MulticlassPrecisionRecall):
-    def __init__(self, output_transform=lambda x: x, label_idx_of_interest=0):
-        super(MulticlassF1, self).__init__(output_transform=output_transform, label_idx_of_interest=label_idx_of_interest)
+    def __init__(self, output_transform=lambda x: x, n_classes=2):
+        super(MulticlassF1, self).__init__(output_transform=output_transform, n_classes=n_classes)
 
     def compute(self):
-        prec = self.tp / (self.tp + self.fp) if (self.tp + self.fp) != 0 else None
-        rec = self.tp / (self.tp + self.fn) if (self.tp + self.fn) != 0 else None
+        results = []
+        for i in range(self.n_classes):
+            tp = self.class_count[i].tp
+            fn = self.class_count[i].fn
+            fp = self.class_count[i].fp
 
-        if (prec == 0 and rec == 0) or prec is None or rec is None:
-            return np.nan
-        else:
-            return (prec * rec * 2 / (prec + rec))
+            prec = tp / (tp + fp) if (tp + fp) != 0 else None
+            rec = tp / (tp + fn) if (tp + fn) != 0 else None
 
-class MulticlassSingleClassAccuracy(MulticlassPrecisionRecall):
-    def __init__(self, output_transform=lambda x: x, label_idx_of_interest=0):
-        super(MulticlassSingleClassAccuracy, self).__init__(output_transform=output_transform, label_idx_of_interest=label_idx_of_interest)
+            if (prec == 0 and rec == 0) or prec is None or rec is None:
+                results.append(np.nan)
+            else:
+                results.append(prec * rec * 2 / (prec + rec))
+        return results
 
-    def compute(self):
-        if (self.tp + self.tn + self.fp + self.fn) == 0:
-            return np.nan
-        else:
-            return (self.tp + self.tn) / (self.tp + self.tn + self.fp + self.fn)
 
-class MulticlassAccuracy(Metric):
-    def __init__(self, output_transform=lambda x: x):
-        self.correct = 0
-        self.all = 0
-        self.sigmoid_fn = torch.nn.Sigmoid()
-        super(MulticlassAccuracy, self).__init__(output_transform=output_transform)
-
-    def reset(self):
-        self.correct = 0
-        self.all = 0
-
-    def update(self, output):
-        y_pred, y = output
-        y_pred = self.sigmoid_fn(y_pred)
-        y_pred = y_pred.round()
-        self.correct += int((y_pred == y).sum())
-        self.all += int(y.shape[0] * y.shape[1])
+class MulticlassAccuracy(MulticlassPrecisionRecall):
+    def __init__(self, output_transform=lambda x: x, n_classes=2):
+        super(MulticlassAccuracy, self).__init__(output_transform=output_transform, n_classes=n_classes)
 
     def compute(self):
-        if self.all == 0:
-            return np.nan
-        else:
-            return self.correct / self.all
+        results = []
+        hits = 0
+        misses = 0
+        for i in range(self.n_classes):
+            tp = self.class_count[i].tp
+            fn = self.class_count[i].fn
+            fp = self.class_count[i].fp
+            tn = self.class_count[i].tn
+
+            hits += tp + tn
+            misses += fp + fn
+
+            if (tp + tn + fp + fn) == 0:
+                results.append(np.nan)
+            else:
+                results.append((tp + tn) / (tp + tn + fp + fn))
+
+        overall_accuracy = hits / (hits + misses) if hits + misses > 0 else np.nan
+
+        return {"per_class": results, "overall": overall_accuracy}
+
+class MulticlassOverallAccuracy(MulticlassAccuracy):
+    def __init__(self, output_transform=lambda x: x, n_classes=2):
+        super(MulticlassOverallAccuracy, self).__init__(output_transform=output_transform, n_classes=n_classes)
+
+    def compute(self):
+        return self.compute()["overall"]
+
+class MulticlassPerClassAccuracy(MulticlassAccuracy):
+    def __init__(self, output_transform=lambda x: x, n_classes=2):
+        super(MulticlassOverallAccuracy, self).__init__(output_transform=output_transform, n_classes=n_classes)
+
+    def compute(self):
+        return self.compute()["per_class"]
 
 def create_eval_engine(model, is_multilabel, n_classes, cpu):
 
@@ -148,17 +188,16 @@ def create_eval_engine(model, is_multilabel, n_classes, cpu):
   eval_engine = Engine(process_function)
 
   if is_multilabel:
-      accuracy = MulticlassAccuracy()
+      accuracy = MulticlassOverallAccuracy(n_classes=n_classes)
       accuracy.attach(eval_engine, "accuracy")
-      for i in range(n_classes):
-          accuracy = MulticlassSingleClassAccuracy(label_idx_of_interest=i)
-          accuracy.attach(eval_engine, f"accuracy{str(n_classes)}")
-          recall = MulticlassRecall(label_idx_of_interest=i)
-          recall.attach(eval_engine, f"recall{str(n_classes)}")
-          precision = MulticlassPrecision(label_idx_of_interest=i)
-          precision.attach(eval_engine, f"precision{str(n_classes)}")
-          f1 = MulticlassF1(label_idx_of_interest=i)
-          f1.attach(eval_engine, f"f1{str(n_classes)}")
+      per_class_accuracy = MulticlassPerClassAccuracy(n_classes=n_classes)
+      per_class_accuracy.attach(eval_engine, f"per class accuracy")
+      recall = MulticlassRecall(n_classes=n_classes)
+      recall.attach(eval_engine, f"recall")
+      precision = MulticlassPrecision(n_classes=n_classes)
+      precision.attach(eval_engine, f"precision")
+      f1 = MulticlassF1(n_classes=n_classes)
+      f1.attach(eval_engine, f"f1")
   else:
       accuracy = Accuracy()
       accuracy.attach(eval_engine, "accuracy")
