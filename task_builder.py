c73abc8c1fc7c3293b444adf0500b50b0a9f5386
Peter Devine <pdev438@UoA.auckland.ac.nz>
2020-03-16 18:01:27 +1300

    Add di sorbo and guzman to tasks

--- task_builder.py
+++ task_builder.py
@@ -1,5 +1,6 @@
 import requests, zipfile, io, os, json, shutil
 import pandas as pd
+from bs4 import BeautifulSoup
 from task import Task
 from models import get_language_model
 
@@ -15,7 +16,9 @@ class TaskBuilder:
             "maalej_2015_user_bin_multilabel": Task(data_getter_fn=self.get_maalej_2015_user_bin_multilabel, is_multilabel=True),
             "chen_2014_swiftkey": Task(data_getter_fn=self.get_chen_2014_swiftkey, is_multilabel=False),
             "ciurumelea_2017_fine": Task(data_getter_fn=self.get_ciurumelea_2017_fine, is_multilabel=True),
-            "ciurumelea_2017_coarse": Task(data_getter_fn=self.get_ciurumelea_2017_coarse, is_multilabel=True)
+            "ciurumelea_2017_coarse": Task(data_getter_fn=self.get_ciurumelea_2017_coarse, is_multilabel=True),
+            "di_sorbo_2017": Task(data_getter_fn=self.get_di_sorbo_2017, is_multilabel=False),
+            "guzman_2015": Task(data_getter_fn=self.get_guzman_2015, is_multilabel=False)
         }
         self.data_path = "./data"
 
@@ -203,4 +206,81 @@ class TaskBuilder:
         val = train_and_val.drop(train.index)
         test = df.drop(train_and_val.index)
 
+        return train, val, test
+
+    def get_di_sorbo_2017(self):
+
+        task_data_path = os.path.join(self.data_path, "di_sorbo_2017")
+        # from https://www.merlin.uzh.ch/contributionDocument/download/9373
+        # What Would Users Change in My App? Summarizing App Reviews for Recommending Software Changes
+        zip_file_path = os.path.join(task_data_path, "SURF-SURF-v.1.0.zip")
+        if not os.path.exists(zip_file_path):
+            r = requests.get("https://zenodo.org/record/47323/files/SURF-SURF-v.1.0.zip?download=1")
+            z = zipfile.ZipFile(io.BytesIO(r.content))
+            z.extractall(path=task_data_path)
+
+        file_dir = os.path.join(task_data_path, "SURF-SURF-v.1.0", "panichella-SURF-29332ec", "SURF_replication_package", "Experiment I", "summaries")
+
+        def add_data_to_df(data, df):
+            soup = BeautifulSoup(data, 'html.parser')
+
+            label_upper_element = [x for x in soup.find_all("sup")]
+            text_list = [x.findNext('a').text for x in label_upper_element]
+            label_list = [x.find('b').text for x in label_upper_element]
+
+            full_review_df = pd.DataFrame({"text": text_list, "label": label_list})
+
+            if df is None:
+                df = full_review_df
+            else:
+                df = df.append(full_review_df)
+
+            return df
+
+        # Cycle through all the app files and add the review text and labels to the overall dataframe
+        all_review_df = None
+        for file_name in os.listdir(file_dir):
+            with open(os.path.join(file_dir, file_name), "r") as f:
+                data = f.read()
+                all_review_df = add_data_to_df(data, all_review_df)
+
+        train_and_val = all_review_df.sample(n=438, random_state=self.random_state)
+        train = train_and_val.sample(frac=0.7, random_state=self.random_state)
+        val = train_and_val.drop(train.index)
+        test = all_review_df.drop(train_and_val.index)
+
+        return train, val, test
+
+    def get_guzman_2015(self):
+        df = pd.read_csv("https://ase.in.tum.de/lehrstuhl_1/images/publications/Emitza_Guzman_Ortega/truthset.tsv",
+                         sep="\t", names=[0, "label", 2, "app", 4, "text"])
+
+        int_to_str_label_map = {
+            5: "Praise",
+            3: "Feature shortcoming",
+            1: "Bug report",
+            2: "Feature strength",
+            7: "Usage scenario",
+            4: "User request",
+            6: "Complaint",
+            8: "Noise"
+        }
+        df["label"] = df.label.apply(lambda x: int_to_str_label_map[x])
+
+        int_to_app_name_map = {
+            6: "Picsart",
+            8: "Whatsapp",
+            7: "Pininterest",
+            1: "Angrybirds",
+            3: "Evernote",
+            5: "Tripadvisor",
+            2: "Dropbox"
+        }
+
+        df["app"] = df.app.apply(lambda x: int_to_app_name_map[x])
+
+        train_and_val = df.sample(frac=0.8, random_state=self.random_state)
+        train = train_and_val.sample(frac=0.7, random_state=self.random_state)
+        val = train_and_val.drop(train.index)
+        test = df.drop(train_and_val.index)
         return train, val, test
