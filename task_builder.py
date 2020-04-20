32105a51310bdb67b1f2039fe260364caccfcd58
Peter-UCL <uceipa0@ucl.ac.uk>
2020-04-20 17:57:11 +1200

    Add tizard 2019 to project and remove zip file paths (as they are functionally useless)

--- task_builder.py
+++ task_builder.py
@@ -1,5 +1,6 @@
 import requests, zipfile, io, os, json, shutil
 from io import StringIO
+import sqlite3
 import pandas as pd
 from bs4 import BeautifulSoup
 from task import Task
@@ -19,6 +20,7 @@ class TaskBuilder:
             "scalabrino_2017": Task(data_getter_fn=self.get_scalabrino_2017, is_multilabel=False),
             "jha_2017": Task(data_getter_fn=self.get_jha_2017, is_multilabel=False),
             "morales_ramirez_2019": Task(data_getter_fn=self.get_morales_ramirez_2019, is_multilabel=False),
+            "tizard_2019": Task(data_getter_fn=self.get_tizard_2019, is_multilabel=False),
         }
         self.data_path = "./data"
 
@@ -42,11 +44,9 @@ class TaskBuilder:
         task_data_path = os.path.join(self.data_path, "maalej_2016")
         # from https://mast.informatik.uni-hamburg.de/wp-content/uploads/2015/06/review_classification_preprint.pdf
         # Bug Report, Feature Request, or Simply Praise? On Automatically Classifying App Reviews
-        zip_file_path = os.path.join(task_data_path, "REJ_data.zip")
-        if not os.path.exists(zip_file_path):
-            r = requests.get("https://mast.informatik.uni-hamburg.de/wp-content/uploads/2014/03/REJ_data.zip")
-            z = zipfile.ZipFile(io.BytesIO(r.content))
-            z.extractall(path = task_data_path)
+        r = requests.get("https://mast.informatik.uni-hamburg.de/wp-content/uploads/2014/03/REJ_data.zip")
+        z = zipfile.ZipFile(io.BytesIO(r.content))
+        z.extractall(path = task_data_path)
 
         json_path = os.path.join(task_data_path, "REJ_data", "all.json")
 
@@ -102,11 +102,9 @@ class TaskBuilder:
         task_data_path = os.path.join(self.data_path, "williams_2017")
         # from
         # Mining Twitter feeds for software user requirements.
-        zip_file_path = os.path.join(task_data_path, "re17.zip")
-        if not os.path.exists(zip_file_path):
-            r = requests.get("http://seel.cse.lsu.edu/data/re17.zip")
-            z = zipfile.ZipFile(io.BytesIO(r.content))
-            z.extractall(path = task_data_path)
+        r = requests.get("http://seel.cse.lsu.edu/data/re17.zip")
+        z = zipfile.ZipFile(io.BytesIO(r.content))
+        z.extractall(path = task_data_path)
 
         file_path = os.path.join(task_data_path, "RE17", "tweets_full_dataset.dat")
 
@@ -158,11 +156,9 @@ class TaskBuilder:
         task_data_path = os.path.join(self.data_path, "chen_2014")
         # from https://ink.library.smu.edu.sg/cgi/viewcontent.cgi?article=3323&context=sis_research
         # AR-Miner: Mining Informative Reviews for Developers from Mobile App Marketplace
-        zip_file_path = os.path.join(task_data_path, "datasets.zip")
-        if not os.path.exists(zip_file_path):
-            r = requests.get("https://sites.google.com/site/appsuserreviews/home/datasets.zip?attredirects=0&d=1")
-            z = zipfile.ZipFile(io.BytesIO(r.content))
-            z.extractall(path=task_data_path)
+        r = requests.get("https://sites.google.com/site/appsuserreviews/home/datasets.zip?attredirects=0&d=1")
+        z = zipfile.ZipFile(io.BytesIO(r.content))
+        z.extractall(path=task_data_path)
 
         def df_getter(data_path, label):
             with open(data_path, "r") as f:
@@ -230,11 +226,9 @@ class TaskBuilder:
         task_data_path = os.path.join(self.data_path, "ciurumelea_2017")
         # from https://ieeexplore.ieee.org/stamp/stamp.jsp?tp=&arnumber=7884612&tag=1
         # Analyzing Reviews and Code of Mobile Apps for Better Release Planning
-        zip_file_path = os.path.join(task_data_path, "UserReviewReference-Replication-Package-URR-v1.0.zip")
-        if not os.path.exists(zip_file_path):
-            r = requests.get("https://zenodo.org/record/161842/files/panichella/UserReviewReference-Replication-Package-URR-v1.0.zip?download=1")
-            z = zipfile.ZipFile(io.BytesIO(r.content))
-            z.extractall(path=task_data_path)
+        r = requests.get("https://zenodo.org/record/161842/files/panichella/UserReviewReference-Replication-Package-URR-v1.0.zip?download=1")
+        z = zipfile.ZipFile(io.BytesIO(r.content))
+        z.extractall(path=task_data_path)
 
         review_data_path = os.path.join(task_data_path, "panichella-UserReviewReference-Replication-Package-643afe0", "data", "reviews", "golden_set.csv")
 
@@ -273,11 +267,9 @@ class TaskBuilder:
         task_data_path = os.path.join(self.data_path, "di_sorbo_2017")
         # from https://www.merlin.uzh.ch/contributionDocument/download/9373
         # What Would Users Change in My App? Summarizing App Reviews for Recommending Software Changes
-        zip_file_path = os.path.join(task_data_path, "SURF-SURF-v.1.0.zip")
-        if not os.path.exists(zip_file_path):
-            r = requests.get("https://zenodo.org/record/47323/files/SURF-SURF-v.1.0.zip?download=1")
-            z = zipfile.ZipFile(io.BytesIO(r.content))
-            z.extractall(path=task_data_path)
+        r = requests.get("https://zenodo.org/record/47323/files/SURF-SURF-v.1.0.zip?download=1")
+        z = zipfile.ZipFile(io.BytesIO(r.content))
+        z.extractall(path=task_data_path)
 
         file_dir = os.path.join(task_data_path, "panichella-SURF-29332ec", "SURF_replication_package", "Experiment I", "summaries")
 
@@ -350,11 +342,9 @@ class TaskBuilder:
         task_data_path = os.path.join(self.data_path, "jha_2017")
         # from https://www.springer.com/content/pdf/10.1007%2F978-3-319-54045-0.pdf
         # Mining User Requirements from Application Store Reviews Using Frame Semantics
-        zip_file_path = os.path.join(task_data_path, "refsq17.zip")
-        if not os.path.exists(zip_file_path):
-            r = requests.get("http://seel.cse.lsu.edu/data/refsq17.zip")
-            z = zipfile.ZipFile(io.BytesIO(r.content))
-            z.extractall(path=task_data_path)
+        r = requests.get("http://seel.cse.lsu.edu/data/refsq17.zip")
+        z = zipfile.ZipFile(io.BytesIO(r.content))
+        z.extractall(path=task_data_path)
 
         def get_jha_df(filename):
             review_data_path = os.path.join(task_data_path, "refsq17", "refsq17", "BOW", filename)
@@ -427,6 +417,82 @@ class TaskBuilder:
 
         # We append the string "_(BUG)" to all "DEFECT" labels so that we can search for the word "bug" in all labels when doing zero-shot evaluation
         df.label = df.label.apply(lambda x: f"{x}_(BUG)" if "defect" in x.lower() else x)
+
+        train_and_val = df.sample(frac=0.7, random_state=self.random_state)
+        train = train_and_val.sample(frac=0.7, random_state=self.random_state)
+        val = train_and_val.drop(train.index)
+        test = df.drop(train_and_val.index)
+
+        return train, val, test
+
+    def get_tizard_2019(self):
+        task_data_path = os.path.join(self.data_path, "tizard_2019")
+
+        r = requests.get("https://zenodo.org/record/3340156/files/RE_Submission_17-master.zip?download=1")
+        z = zipfile.ZipFile(io.BytesIO(r.content))
+        z.extractall(path=task_data_path)
+
+        db = sqlite3.connect(os.path.join(task_data_path, "RE_Submission_17-master", "VLC_labelled_sentences_4RE.sqlite"))
+
+        column_names = [x[1] for x in db.execute("""PRAGMA table_info('labelled_sentences');""")]
+
+        row_data = [row for row in db.execute("SELECT * from labelled_sentences")]
+
+        df = pd.DataFrame(row_data, columns=column_names)
+
+        bad_label_list = [
+            "and Remove Cookies Warning\xa0! ",
+            "and Remove Cookies  ",
+            "remove",
+            "continued from previous"
+        ]
+
+        # Remove the four rows with mistake labels
+        df = df.loc[~df.label.apply(lambda x: x in bad_label_list)]
+
+        def forum_label_transformer(raw_label):
+            lower_label = raw_label.lower()
+
+            if "application guidance" in lower_label:
+                return "application guidance"
+            if "non-informative" in lower_label:
+                return "non-informative"
+            if "apparent bug" in lower_label:
+                return "apparent bug"
+            if "question on application" in lower_label:
+                return "question on application"
+            if "feature request" in lower_label:
+                return "feature request"
+            if "help seek" in lower_label:
+                return "help seeking"
+            if "user setup" in lower_label:
+                return "user setup"
+            if "usage" in lower_label:
+                return "application usage"
+            if "is-background" in lower_label:
+                return "question on background"
+            if "attempted solution" in lower_label:
+                return "attempted solution"
+            if "requesting" in lower_label:
+                return "requesting more information"
+            if "dispraise" in lower_label:
+                return "dispraise for application"
+            if "praise application" in lower_label:
+                return "praise for application"
+            if "acknowledgement" in lower_label:
+                return "acknowledgement of problem resolution"
+            if "agreeing with the problem" in lower_label:
+                return "agreeing with the problem"
+            if "limitation confirmation" in lower_label:
+                return "limitation confirmation"
+            if "bug confirmation" in lower_label:
+                return "bug confirmation"
+            if "agreeing with the request" in lower_label:
+                return "agreeing with the feature request"
+            return "other"
+
+        # Map all labels to their lower-case proper label. Mark all labels outside of the literature label set as "other".
+        df.label = df.label.apply(forum_label_transformer)
 
         train_and_val = df.sample(frac=0.7, random_state=self.random_state)
         train = train_and_val.sample(frac=0.7, random_state=self.random_state)
