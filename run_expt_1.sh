52f1f606e0c20ed803f652b0c6eb52a174375d14
Peter Devine <pdev438@aucklanduni.ac.nz>
2020-04-28 15:54:15 +1200

    Add bash script to run expertiment 1 across all datasets

---
+++ run_expt_1.sh
@@ -0,0 +1,14 @@
+datasets=(jha_2017 scalabrino_2017 maalej_2016 williams_2017 di_sorbo_2017 guzman_2015 tizard_2019)
+random_states=(1,2,3,4,5,6,7,8,9,10)
+
+export NEPTUNE_API_TOKEN=$2
+
+for dataset in ${datasets[@]}; do
+	for random_state in ${random_states[@]}; do
+	        python software_feedback_cls.py --dataset_list=$dataset --random_state=$random_state --output_text --num_epochs=60 --num_fine_tuning_epochs=60 --batch_size_train=24 --batch_size_eval=24 --max_length=128 --neptune_username=$1
+	done
+done
+
+python overall_experiment_data_collector.py --experiment_name=experiment_1_all_datasets --neptune_username=$1
+
+#python software_feedback_cls.py --dataset_list=jha_2017,scalabrino_2017,maalej_2016,williams_2017,guzman_2015,tizard_2019 --output_text --zero_shot_dataset_list=jha_2017,scalabrino_2017,maalej_2016,williams_2017,di_sorbo_2017,guzman_2015,tizard_2019 --num_epochs=60 --num_fine_tuning_epochs=60 --batch_size_train=8 --batch_size_eval=8 --max_length=128 --zero_shot_label=bug --neptune_username=$1
