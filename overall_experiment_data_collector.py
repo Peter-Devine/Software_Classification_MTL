95fd9217b6d59be9880e458421de0fe14ea8771a
Peter Devine <pdev438@aucklanduni.ac.nz>
2020-05-01 14:34:22 +1200

    Make experiment 1 bash output overall results

--- overall_experiment_data_collector.py
+++ overall_experiment_data_collector.py
@@ -2,9 +2,10 @@ import argparse
 from logger import NeptuneLogger
 
 parser = argparse.ArgumentParser()
+parser.add_argument('--experiment_number', default=0, type=int, help='Which experiment number do you want to collect data for 1(A>A),2(A>X),3(A&B>A&B) or 4(A&B>X)')
 parser.add_argument('--experiment_name', default="", type=str, help='What shall we call the overall experiment?')
 parser.add_argument('--neptune_username', default="", type=str, help=' (Optional) For outputting training/eval metrics to neptune.ai. Valid neptune username. Not your neptune.ai API key must also be stored as $NEPTUNE_API_TOKEN environment variable.')
 args = parser.parse_args()
 
 logger = NeptuneLogger(args.neptune_username)
-logger.log_output_files(experiment_name = args.experiment_name)
+logger.log_output_files(experiment_name = args.experiment_name, experiment_number=args.experiment_number)
--- run_expt_1.sh
+++ run_expt_1.sh
@@ -3,14 +3,19 @@ random_states=(1 2 3 4 5 6 7 8 9 10)
 
 export NEPTUNE_API_TOKEN=$2
 
+# Delete all output files from previous runs
+python output_clean.py
+
+# Run experiment 1 on all datasets for 10 different random seeds
 for dataset in ${datasets[@]}; do
 	for random_state in ${random_states[@]}; do
 					echo "Now running dataset $dataset in random state $random_state"
 
-	        python software_feedback_cls.py --dataset_list=$dataset --random_state=$random_state --output_text --num_epochs=60 --num_fine_tuning_epochs=60 --batch_size_train=24 --batch_size_eval=24 --max_length=128 --neptune_username=$1
+	        python software_feedback_cls.py --dataset_list=$dataset --random_state=$random_state --output_text --do_classical --num_epochs=60 --num_fine_tuning_epochs=60 --batch_size_train=24 --batch_size_eval=24 --max_length=128 --neptune_username=$1
 	done
 done
 
-python overall_experiment_data_collector.py --experiment_name=experiment_1_all_datasets --neptune_username=$1
+# Collect the generated output files and output them to a meta-experiment on Neptune
+python overall_experiment_data_collector.py --experiment_number=1 --experiment_name=experiment_1_all_datasets --neptune_username=$1
 
 #python software_feedback_cls.py --dataset_list=jha_2017,scalabrino_2017,maalej_2016,williams_2017,guzman_2015,tizard_2019 --output_text --zero_shot_dataset_list=jha_2017,scalabrino_2017,maalej_2016,williams_2017,di_sorbo_2017,guzman_2015,tizard_2019 --num_epochs=60 --num_fine_tuning_epochs=60 --batch_size_train=8 --batch_size_eval=8 --max_length=128 --zero_shot_label=bug --neptune_username=$1
