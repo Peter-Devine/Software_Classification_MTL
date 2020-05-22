167569646ab0d62c70be37a509fce768d24b854b
unknown <pdev438@aucklanduni.ac.nz>
2020-05-22 15:10:46 +1200

    Output in/out class probs instead of max arg pred in zero shot and prevent DIV(0) errors in f1 calc

--- zero_shot.py
+++ zero_shot.py
@@ -1,3 +1,5 @@
+import torch
+
 from ignite.engine import Engine
 from ignite.metrics import Accuracy, Recall, Precision, ConfusionMatrix
 from ignite.metrics import Metric
@@ -27,26 +29,35 @@ class LMZeroShot:
 
         def process_function(engine, batch):
             X, y = batch
-            # pred = model(X.cuda())
-            # gold = y.cuda()
+
             if cpu:
                 pred = model(X.cpu())
-                # Get the argmax of the raw model output (logits)
-                _, pred = pred.max(1)
                 gold = y.cpu()
             else:
-
                 pred = model(X.cuda())
-                # # Get the argmax of the raw model output (logits)
-                _, pred = pred.max(1)
                 gold = y.cuda()
 
-            pred = (pred == model_target_int).long()
+            # Get the softmax of the raw model output (logits)
+            pred = torch.softmax(pred, dim=1)
+
+            # Get the probability that the prediction is the target class
+            pred_in_class_prob = pred[:,[model_target_int]]
+
+            # Get all the probabilities of all the other classes outside the target class by finding the complement of the in class probability
+            pred_out_class_prob = 1 - pred_in_class_prob
+
+            # Create a combined tensor which acts as a set of probabilities for in vs out of the zero-shot target class.
+            # In this, 0 is out of class, whilst 1 is in class, so the combined tensor has the out of class probabilities in the 0th column and the in-class probs in the 1st column.
+            pred = torch.cat((pred_out_class_prob, pred_in_class_prob), dim=1)
+
+            # To correspond to the above contructed tensor, we set the golds as 1 (I.e. True) if the gold label is the zero-shot label, and 0 (False) if not.
             gold = (gold == label_target_int).long()
 
             return pred, gold
 
         eval_engine = Engine(process_function)
+
+        really_small_number = 1e-10
 
         accuracy = Accuracy()
         accuracy.attach(eval_engine, "accuracy")
@@ -54,18 +65,18 @@ class LMZeroShot:
         recall.attach(eval_engine, "recall")
         precision = Precision()
         precision.attach(eval_engine, "precision")
-        # f1 = (precision * recall * 2 / (precision + recall))
-        # f1.attach(eval_engine, "f1")
-        # f2 = (precision * recall * 5 / ((4*precision) + recall))
-        # f2.attach(eval_engine, "f2")
+        f1 = (precision * recall * 2 / (precision + recall + really_small_number))
+        f1.attach(eval_engine, "f1")
+        f2 = (precision * recall * 5 / ((4*precision) + recall + really_small_number))
+        f2.attach(eval_engine, "f2")
 
         avg_recall = Recall(average=True)
         avg_recall.attach(eval_engine, "average recall")
         avg_precision = Precision(average=True)
         avg_precision.attach(eval_engine, "average precision")
-        avg_f1 = (avg_precision * avg_recall * 2 / (avg_precision + avg_recall))
+        avg_f1 = (avg_precision * avg_recall * 2 / (avg_precision + avg_recall + really_small_number))
         avg_f1.attach(eval_engine, "average f1")
-        avg_f2 = (avg_precision * avg_recall * 5 / ((4*avg_precision) + avg_recall))
+        avg_f2 = (avg_precision * avg_recall * 5 / ((4*avg_precision) + avg_recall + really_small_number))
         avg_f2.attach(eval_engine, "average f2")
 
         return eval_engine
